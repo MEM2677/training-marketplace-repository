f3c984acd85a3df8e59664f441ac003cee9c2df8
Daniel Vilas <daniel.vilas@gmail.com>
2018-07-12 08:48:21 +0200

    Daniel's Plugin

---
+++ everis-component/plugins/everis-plugin-jpdvilas/src/main/resources/sql/plugins/jpdvilas/port_data_production.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgetcatalog(code, titles, parameters, plugincode, parenttypecode, defaultconfig, locked, maingroup) VALUES ('DanielWidget', '<?xml version="1.0" encoding="UTF-8"?>
+<properties>
+<property key="en">PAM-Case comments</property>
+<property key="it">Commenti del caso PAM</property>
+</properties>', null, 'jpdvilas', NULL, NULL, 1, 'free');
+
